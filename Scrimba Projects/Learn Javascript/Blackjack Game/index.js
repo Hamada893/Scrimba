668d2934086b39f1a2029e02210d277de7c36ecf
Hamada <hamadauter@gmail.com>
2025-01-24 21:04:01 +0300

    Added second button function (still raw)

--- Scrimba Projects/Learn Javascript/Blackjack Game/index.js
+++ Scrimba Projects/Learn Javascript/Blackjack Game/index.js
@@ -9,8 +9,8 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    sumEl.textContent = "Sum: " + sum
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -23,3 +23,7 @@ function startGame() {
     messageEl.textContent = message
 }
 
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+}
