3c07b668efbe9a8e9dcdaf3f3f37b58b42dd42a9
Hamada <hamadauter@gmail.com>
2025-01-25 15:19:47 +0300

    Fixed a bug: console pre-determines card values before game start

--- Scrimba Projects/Learn Javascript/Blackjack Game/index.js
+++ Scrimba Projects/Learn Javascript/Blackjack Game/index.js
@@ -1,34 +1,33 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
 function getRandomCard() {
-    
-    let card = Math.floor(Math.random() * 13) + 1;
-
-    
-    if (card === 1) {
-        return 11;
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
     }
-
-    
-    if (card >= 11 && card <= 13) {
-        return 10;
-    }
-
-    // Otherwise return card as is
-    return card;
 }
 
 function startGame() {
+    isAlive = true
+    // Generate two random numbers
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    
+    // Re-assign the cards and sum variables so that the game can start
+    
     renderGame()
 }
 
