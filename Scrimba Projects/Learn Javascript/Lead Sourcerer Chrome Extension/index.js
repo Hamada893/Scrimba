a998b9b35a3c3b1afacb2359eca2db6424ba63dc
Hamada <hamadauter@gmail.com>
2025-01-27 03:52:27 +0300

    Update index.js

--- Scrimba Projects/Learn Javascript/Lead Sourcerer Chrome Extension/index.js
+++ Scrimba Projects/Learn Javascript/Lead Sourcerer Chrome Extension/index.js
@@ -3,11 +3,15 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// Get the leads from the localStorage - PS: JSON.parse()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
     
