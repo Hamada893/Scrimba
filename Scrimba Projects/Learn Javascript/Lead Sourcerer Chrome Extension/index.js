aa650243d479939fba06ddcd47dd147a5baaa1c0
Hamada <hamadauter@gmail.com>
2025-01-27 03:04:30 +0300

    Added template strings for the first time

--- Scrimba Projects/Learn Javascript/Lead Sourcerer Chrome Extension/index.js
+++ Scrimba Projects/Learn Javascript/Lead Sourcerer Chrome Extension/index.js
@@ -5,18 +5,20 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    // 2. Call the renderLeads() function
-    renderLeads()
     inputEl.value = ""
+    renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
-ulEl.innerHTML = listItems
-}
-
-
