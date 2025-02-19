81366c6f84350c4cde4e7758b69405c3f7f61821
Hamada <hamadauter@gmail.com>
2025-02-19 04:39:39 +0300

    Update index.js

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
@@ -1,27 +1,25 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+let modalTxt = document.getElementById('modal-text')
 
-setTimeout(function(){
+setTimeout(function() {
     modal.style.display = 'inline'
 }, 1500)
 
-modalCloseBtn.addEventListener('click', function(){
+modalCloseBtn.addEventListener('click', function() {
     modal.style.display = 'none'
 })
 
-/*
-Challenge:
-1. Take control of the form element. 
-2. Add an eventListener to the form to listen for a
-   "submit" event.
-3. When a user clicks "accept", prevent the default
-   behaviour that triggers the refresh.
-4. Log out "form submitted".
-*/  
-
-consentForm.addEventListener('submit', function(e){
+consentForm.addEventListener('submit', function(e) {
     e.preventDefault()
-    console.log('form submitted')
-})
 
+    modalTxt.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="uploadText">
+                Uploading your data to the dark web...
+            </p>
+        </div>
+    `
+})
