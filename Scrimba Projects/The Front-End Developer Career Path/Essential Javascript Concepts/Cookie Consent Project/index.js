6ab4cbaaee38acdfb7952c5d0921e614b9abcc01
Hamada <hamadauter@gmail.com>
2025-02-19 04:29:45 +0300

    Stopped browser's default behavior which displays given form information in the URL

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
@@ -1,18 +1,27 @@
-/*
-Challenge:
-1. Take control of the close button.
-2. Use an event listener to set the display 
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
-
 const modal = document.getElementById('modal')
-const closeBtn = document.getElementById('modal-close-btn')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
 
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
 
-closeBtn.addEventListener('click', function() {
+modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
+
+/*
+Challenge:
+1. Take control of the form element. 
+2. Add an eventListener to the form to listen for a
+   "submit" event.
+3. When a user clicks "accept", prevent the default
+   behaviour that triggers the refresh.
+4. Log out "form submitted".
+*/  
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('form submitted')
+})
+
