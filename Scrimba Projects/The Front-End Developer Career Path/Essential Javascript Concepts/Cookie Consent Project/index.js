eb4214fa2da6c65b1a0eacf40001c53db8420f8a
Hamada <hamadauter@gmail.com>
2025-02-19 03:35:50 +0300

    Made the setTimeout function actually work

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
@@ -1,9 +1,12 @@
 /*
 Challenge:
-1. Log out the phrase "Modal Opened!" 
-   after a 1.5 second delay.
+1. Take control of the 'modal' div with JavaScript.
+2. Swap out our console.log for a line of code
+   which will change the CSS 'display' property
+   of our modal to 'inline'.
 */
+const modal = document.getElementById('modal')
 
 setTimeout(function(){
-    console.log("Modal Opened!")
+    modal.style.display = 'block'
 }, 1500)
