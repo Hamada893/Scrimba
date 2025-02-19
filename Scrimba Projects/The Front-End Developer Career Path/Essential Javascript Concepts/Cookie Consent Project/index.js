17d7882cf254d1002be520f97d46479ec6bcf003
Hamada <hamadauter@gmail.com>
2025-02-19 05:06:17 +0300

    Update index.js

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Cookie Consent Project/index.js
@@ -20,8 +20,19 @@ consentForm.addEventListener('submit', function(e){
     </div>` 
     
     setTimeout(function(){
-        document.getElementById('upload-text').innerText = "Making the sale..."
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
     }, 1500)
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    ` 
+    }, 3000)
   
 /*   
 Challenge: 
@@ -37,13 +48,5 @@ Challenge:
     </div>
     ` 
 */  
-    setTimeout(function(){
-        modal.innerHTML = `
-        <h2>Thanks you sucker! </h2>
-    <p>We just sold the rights to your eternal soul.</p>
-    <div class="idiot-gif">
-        <img src="images/pirate.gif">
-    </div>
-    ` 
-    }, 3000)
+
 }) 
