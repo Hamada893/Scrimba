163026f0de934c255935992ad8f129418e6891b9
Hamada <hamadauter@gmail.com>
2025-02-21 02:30:26 +0300

    Implemented the ability to know which button was clicked by using the 'change' event

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
@@ -2,13 +2,15 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-function getEmotionsArray(cats) {
-    const emotionsArray = []
+emotionRadios.addEventListener('change', function(e){
+    console.log(e.target.id)
+})
 
-    for (let cat of cats) {
-        for (let emotion of cat.emotionTags) {
-
-            if (!emotionsArray.includes(emotion)) {
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
                 emotionsArray.push(emotion)
             }
         }
@@ -17,11 +19,11 @@ function getEmotionsArray(cats) {
 }
 
 
-function renderEmotionsRadios(cats) {
-
+function renderEmotionsRadios(cats){
+        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions) {
+    for (let emotion of emotions){
         radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
