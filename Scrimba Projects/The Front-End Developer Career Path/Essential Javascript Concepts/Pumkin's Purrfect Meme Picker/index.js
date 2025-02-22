4546d9abc5c88c7d2d598ec6c17d40e3fea4bbf9
Hamada <hamadauter@gmail.com>
2025-02-22 15:51:36 +0300

    Update index.js to use return instead of console logging

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
@@ -39,11 +39,11 @@ function getSingleCatObject(){
     const catsArray = getMatchingCatsArray()
     
     if (catsArray.length === 1){
-        console.log(catsArray[0])
+        return catsArray[0]
     }
     else {
         const randomNumber = Math.floor(Math.random() * catsArray.length)
-        console.log(catsArray[randomNumber])
+        return catsArray[randomNumber]
     }
 }
 
