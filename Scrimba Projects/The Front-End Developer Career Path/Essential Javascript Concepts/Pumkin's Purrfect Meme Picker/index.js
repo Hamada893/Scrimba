2936f74d90a616b5066071df34e63ce7056ae0ea
Hamada <hamadauter@gmail.com>
2025-02-22 15:01:22 +0300

    Update index.js to filter out any emotion tags that are not selected

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
@@ -17,13 +17,17 @@ function highlightCheckedOption(e){
 }
 
 
-function getMatchingCatsArray(){
-    const isGif = gifsOnlyOption.checked
-    console.log(isGif)
-  
+function getMatchingCatsArray(){     
     if(document.querySelector('input[type="radio"]:checked')){
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
-        console.log(selectedEmotion)  
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            return cat.emotionTags.includes(selectedEmotion)
+        })
+        
+        console.log(matchingCatsArray)
+
     }  
 }
 
