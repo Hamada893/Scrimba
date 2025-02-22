a1b6714bf9ba3daff7fac9117cd26e63e005d760
Hamada <hamadauter@gmail.com>
2025-02-22 15:43:09 +0300

    Update index.js's logic to check if there only one object with the specified user selected inputs

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Pumkin's Purrfect Meme Picker/index.js
@@ -37,7 +37,10 @@ function getMatchingCatsArray(){
 function getSingleCatObject(){
     
     const catsArray = getMatchingCatsArray()
-    console.log(catsArray)
+    
+    if (catsArray.length === 1){
+        console.log(catsArray[0])
+    }
 
 }
 
