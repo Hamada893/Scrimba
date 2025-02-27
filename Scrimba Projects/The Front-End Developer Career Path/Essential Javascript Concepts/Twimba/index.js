c526406f2f5e967ab971fbf37be25520e460f4b5
Hamada <hamadauter@gmail.com>
2025-02-27 08:30:12 +0300

    Update index.js to render replies if there are any

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Twimba/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Twimba/index.js
@@ -62,10 +62,29 @@ function getFeedHtml(){
             retweetIconClass = 'retweeted'
         }
         
+        let repliesHtml = ''
+        
         if(tweet.replies.length > 0){
             console.log(tweet.uuid)
+
+
+            for (let reply of tweet.replies) {
+               repliesHtml += `
+                        <div class="tweet-reply">
+                <div class="tweet-inner">
+                    <img src="${reply.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${reply.handle}</p>
+                            <p class="tweet-text">${reply.tweetText}</p>
+                        </div>
+                    </div>
+            </div>
+            ` 
+            }
+        
         }
-             
+        
+          
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -95,8 +114,14 @@ function getFeedHtml(){
             </div>   
         </div>            
     </div>
+    <div id="replies-${tweet.uuid}">
+        <!-- REPLIES HERE -->
+        ${repliesHtml}
+    </div>   
 </div>
 `
+
+
    })
    return feedHtml 
 }
