59d0b67e4fe955fe7513c63d5de7fee64860a14b
Hamada <hamadauter@gmail.com>
2025-02-27 09:20:32 +0300

    Added new object to include newly typed tweets dynamically

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Twimba/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Twimba/index.js
@@ -1,6 +1,12 @@
 import { tweetsData } from './data.js'
-const tweetInput = document.getElementById('tweet-input')
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
+/*
+Challenge:
+1. Bring in uuidjs.
+*/
+
+const tweetInput = document.getElementById('tweet-input')
 
 document.addEventListener('click', function(e){
     if(e.target.dataset.like){
@@ -15,7 +21,6 @@ document.addEventListener('click', function(e){
     else if(e.target.id === 'tweet-btn'){
         handleTweetBtnClick()
     }
-
 })
  
 function handleLikeClick(tweetId){ 
@@ -53,7 +58,18 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick(){
-    console.log(tweetInput.value)
+    console.log({
+        handle: `@Scrimba`,
+        profilePic: `images/scrimbalogo.png`,
+        likes: 0,
+        retweets: 0,
+        tweetText: tweetInput.value,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: uuidv4(),
+    },)
+
 }
 
 function getFeedHtml(){
