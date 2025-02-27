98d5ae6c6de7c2b7f0a77b1035e6697cc097b762
Hamada <hamadauter@gmail.com>
2025-02-27 09:32:33 +0300

    Small code refactor

--- Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Twimba/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Essential Javascript Concepts/Twimba/index.js
@@ -7,8 +7,6 @@ Challenge:
    of code to a better position. Find it and move it!
 */
 
-const tweetInput = document.getElementById('tweet-input')
-
 document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
@@ -59,6 +57,7 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
 
     if(tweetInput.value){
         tweetsData.unshift({
