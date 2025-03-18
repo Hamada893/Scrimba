d2eb586ba83f183028cd9e37c08d694579dddc3d
Hamada <hamadauter@gmail.com>
2025-03-18 05:35:37 +0300

    JS now fetches the first 5 posts from the API

--- Scrimba Projects/The Front-End Developer Career Path/Working with APIs/BlogSpace/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Working with APIs/BlogSpace/index.js
@@ -9,4 +9,13 @@
  Since there's so many posts, let's limit the array to just 5 items.
  You can use the `.slice()` array method to just grab the first 5 objects
  from the data array that comes back from the API
+ 
+ Log the 5 items to the console
  */
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        console.log(postsArr)
+    })
