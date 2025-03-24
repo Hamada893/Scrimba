edf71964a43639242759792debdb7264b3476874
Hamada <hamadauter@gmail.com>
2025-03-24 16:37:48 +0300

    Update index.js to fetch 2 new cards when the Draw button is pressed

--- Scrimba Projects/The Front-End Developer Career Path/Working with APIs/War/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Working with APIs/War/index.js
@@ -10,6 +10,12 @@ function handleClick() {
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
 /**
  * Challenge
  * 
@@ -19,4 +25,10 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
  * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
  * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
  * that you'll see in the deck of cards API docs.
+ * 
+ * 1. Create a new button that, when clicked, draws 2 cards from the deckId
+ * you have saved
+ *      Note: you'll need to get a new deck every time you refresh the page,
+ *      since you're only saving your deckId in a local variable right now
+ * 2. Log those 2 cards to the console
  */
