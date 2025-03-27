b540439df10e9faf18eb11221b649e3f1fdc2a38
Hamada <hamadauter@gmail.com>
2025-03-27 23:07:13 +0300

    Added card values and cards now compare values with eachother

--- Scrimba Projects/The Front-End Developer Career Path/Working with APIs/War/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Working with APIs/War/index.js
@@ -28,9 +28,42 @@ drawCardBtn.addEventListener("click", () => {
 /**
  * Challenge:
  * 
- * Place each of the cards we draw into its respective card-slot
- * Hint: consider using element.children in the DOM instead of
- * giving each card-slot its own unique ID
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
  * 
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/children
+ * Part 2:
+ * Instead of logging the winner to the console, 
+ * display an `h2` on the screen above the 2 cards 
+ * that declares who the winner is.
+ * 
+ * If card1 is the higher card, display "Computer wins!"
+ * If card2 is the higher card, display "You win!"
+ * If they're equal, display "War!"
  */
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    console.log("card 1:", card1ValueIndex)
+    console.log("card 2:", card2ValueIndex)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log("Card 1 wins!")
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log("Card 2 wins!")
+    } else {
+        console.log("War!")
+    }
+}
+
+// Keeping this test case for reference, but should delete later
+// const card1Obj = {
+//     value: "JACK"
+// }
+// const card2Obj = {
+//     value: "QUEEN"
+// }
+
+// determineCardWinner(card1Obj, card2Obj)
