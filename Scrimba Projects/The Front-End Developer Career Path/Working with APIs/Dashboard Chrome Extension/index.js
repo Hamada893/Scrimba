cc8ebdeb187b080320debbfeb3c2e88d533ea72a
Hamada <hamadauter@gmail.com>
2025-07-20 19:51:10 +0800

    Update index.js

--- Scrimba Projects/The Front-End Developer Career Path/Working with APIs/Dashboard Chrome Extension/index.js
+++ Scrimba Projects/The Front-End Developer Career Path/Working with APIs/Dashboard Chrome Extension/index.js
@@ -4,66 +4,9 @@ try {
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
 } catch (err) {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-<<<<<<< HEAD
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
     document.getElementById("author").textContent = `By: Dodi Achmad`
 }
-=======
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
-
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
-
-function getCurrentTime() {
-    const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
-}
-
-setInterval(getCurrentTime, 1000)
-
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>
-            `
-        })
-        .catch(err => console.error(err))
-});
->>>>>>> 28eabbbc1bf65c654d07b52d8b067842eb873bdd
-
-/**
- * Challenge: Update the code below and in the 
- * getCurrentLocation callback to use try...catch
- */
 
 try {
     const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
@@ -76,9 +19,9 @@ try {
         <span>${data.name}</span>
     `
     document.getElementById("crypto").innerHTML += `
-    /**    <p>🎯: $${data.market_data.current_price.usd}</p>
-    /**    <p>👆: $${data.market_data.high_24h.usd}</p>
-    /**    <p>👇: $${data.market_data.low_24h.usd}</p>
+        <p>🎯: $${data.market_data.current_price.usd}</p>
+        <p>👆: $${data.market_data.high_24h.usd}</p>
+        <p>👇: $${data.market_data.low_24h.usd}</p>
     `
 } catch (err) {
     console.error(err)
@@ -107,4 +50,4 @@ navigator.geolocation.getCurrentPosition(async position => {
     } catch (err) {
         console.error(err)
     }
-});
+})
