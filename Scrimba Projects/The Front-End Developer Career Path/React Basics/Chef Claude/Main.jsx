c3ed4bcea1c379e35c57fccc6f6acdb893f61589
Mohamed Sameh <hamada@Mohameds-MacBook-Pro.local>
2025-08-17 22:21:46 +0800

    Removed hardcoded placeholder text and wired components and main to contact the real ai API (still needs formatting)

--- Scrimba Projects/The Front-End Developer Career Path/React Basics/Chef Claude/Main.jsx
+++ Scrimba Projects/The Front-End Developer Career Path/React Basics/Chef Claude/Main.jsx
@@ -1,42 +1,16 @@
 import React from "react"
-import ClaudeRecipe from "./ClaudeRecipe"
-import IngredientsList from "./IngredientsList"
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe"
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai"
 
 export default function Main() {
-    /**
-     * Challenge: clean up our code!
-     * Let's make a couple new components to make things a
-     * little cleaner. (Notice: I'm not suggesting what we
-     * have now is bad or wrong. I'm mostly finding an excuse
-     * to get in some hands-on practice 🙂)
-     * 
-     * 1. Move the entire recipe <section> into its own
-     *    ClaudeRecipe component
-     * 2. Move the list of ingredients <section> into its
-     *    own IngredientsList component.
-     * 
-     * While you're considering how to structure things, consider
-     * where state is, think about if it makes sense or not to
-     * move it somewhere else, how you'll communicate between
-     * the parent/child components, etc.
-     * 
-     * The app should function as it currently does when you're
-     * done, so there will likely be some extra work to be done
-     * beyond what I've listed above.
-     */
+    const [ingredients, setIngredients] = React.useState([])
+    const [recipe, setRecipe] = React.useState("")
 
-    const [ingredients, setIngredients] = React.useState(
-        ["all the main spices", "pasta", "ground beef", "tomato paste"]
-    )
-    const [recipeShown, setRecipeShown] = React.useState(false)
-    
-    function toggleRecipeShown() {
-        setRecipeShown(prevShown => !prevShown)
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
+        setRecipe(recipeMarkdown)
     }
-
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
@@ -54,14 +28,15 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            
-            <ClaudeRecipe />
-            <IngredientsList 
-                ingredients={ingredients}
-                ingredientsListItems={ingredientsListItems}
-                toggleRecipeShown={toggleRecipeShown}
-            />
-            
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     )
 }
