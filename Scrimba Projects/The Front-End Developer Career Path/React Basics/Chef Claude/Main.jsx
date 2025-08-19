9b9671783c255024e097b773855c0a7380dfc146
Mohamed Sameh <hamada@Mohameds-MacBook-Pro.local>
2025-08-19 19:27:22 +0800

    Added a sideEffect with useEffect to auto scroll upon recieving the AI recommended recipe

--- Scrimba Projects/The Front-End Developer Career Path/React Basics/Chef Claude/Main.jsx
+++ Scrimba Projects/The Front-End Developer Career Path/React Basics/Chef Claude/Main.jsx
@@ -1,11 +1,20 @@
 import React from "react"
-import IngredientsList from "./components/IngredientsList"
-import ClaudeRecipe from "./components/ClaudeRecipe"
-import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai"
+import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./ClaudeRecipe"
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "../ai"
 
 export default function Main() {
-    const [ingredients, setIngredients] = React.useState([])
+    const [ingredients, setIngredients] = React.useState(
+        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
+    )
     const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+    
+    React.useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView()
+        }
+    }, [recipe])
 
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
@@ -16,7 +25,7 @@ export default function Main() {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-
+    
     return (
         <main>
             <form action={addIngredient} className="add-ingredient-form">
@@ -31,6 +40,7 @@ export default function Main() {
 
             {ingredients.length > 0 &&
                 <IngredientsList
+                    ref={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                 />
