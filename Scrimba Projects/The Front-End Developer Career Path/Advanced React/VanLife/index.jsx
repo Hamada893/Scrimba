0b55bc55337961e8e7f10ba14634d875277e9d42
Mohamed Sameh <hamadauter@gmail.com>
2025-09-14 21:29:24 +0800

    Added an about section and used a React Route for the first time

--- Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/index.jsx
+++ Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/index.jsx
@@ -4,13 +4,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      
-    </Routes>
-  </BrowserRouter>
-);
+
+function Home() {
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>About page goes here! 🎉</h1>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
