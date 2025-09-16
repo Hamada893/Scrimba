44c23fec16bdc7b1d01ed9accf187a38066dff10
Hamada <hamadauter@gmail.com>
2025-09-16 18:36:14 +0800

    Added logic for van details page to fetch data from the params of the visited ID

--- Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/pages/VanDetails.jsx
+++ Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/pages/VanDetails.jsx
@@ -1,5 +1,14 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
-export default function VanDetails() {
+export default function VanDetail() {
+    const params = useParams()
+    
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [params.id])
+    
     return <h1>Van detail page goes here</h1>
 }
