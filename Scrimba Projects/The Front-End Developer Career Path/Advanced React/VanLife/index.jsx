15125d3069d9734ccfad772ea173fe87bea60ae5
Hamada <hamadauter@gmail.com>
2025-09-18 21:54:52 +0800

    Added the host van navigation link and coressponding routes

--- Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/index.jsx
+++ Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/index.jsx
@@ -8,16 +8,18 @@ import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans.jsx"
+import HostVanDetail from "./pages/Host/HostVanDetail"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
 import "./server"
 
-
 function App() {
+
   return (
     <BrowserRouter>
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
@@ -28,6 +30,8 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
