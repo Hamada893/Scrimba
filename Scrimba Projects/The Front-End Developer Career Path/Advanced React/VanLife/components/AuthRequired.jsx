d1a79108b7aa9f6afcbf64d0b115300162838709
Hamada <hamadauter@gmail.com>
2025-09-24 16:13:29 +0800

    Added state to the Authentication required component and used it to display a login first message in the login component

--- Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/components/AuthRequired.jsx
+++ Scrimba Projects/The Front-End Developer Career Path/Advanced React/VanLife/components/AuthRequired.jsx
@@ -1,9 +1,15 @@
-import { Navigate, Outlet } from "react-router-dom"
+import React from "react"
+import { Outlet, Navigate } from "react-router-dom"
 
 export default function AuthRequired() {
     const authenticated = false
+    
     if (!authenticated) {
-        return <Navigate to="/login" />
+        return (
+            <Navigate 
+                to="/login" 
+                state={{message: "You must log in first"}} 
+            />)
     }
     return <Outlet />
 }
