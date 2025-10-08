5391aaa2d13da33d092fa12828e4540c576a92aa
Hamada <hamadauter@gmail.com>
2025-10-09 00:50:57 +0800

    Server now sends JSON stringified data when the route is /api

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/server.js
@@ -1,20 +1,21 @@
 import http from 'node:http'
-
+import { getDataFromDB } from './database/db.js'
+ 
 const PORT = 8000
 
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
 
-const server = http.createServer((req, res) => {
 /*
-Challenge: 
-1. Check the ‘method’ property on the req object.
-   Only serve our string if it’s ‘GET’.
+Challenge:
+  1. Store our data in a const ‘destinations’.
+  2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
+    Think: What changes will you need to make to get this to work?
 */
 
-
   if (req.url === '/api' && req.method === 'GET') {
-    res.end('This is from the server')
+    res.end(JSON.stringify(destinations))
   }
-
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
