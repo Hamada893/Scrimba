ee63a7a9caf5f118905ac2c4af8db8512f39e244
Hamada <hamadauter@gmail.com>
2025-10-09 01:40:55 +0800

    Added a utility function to reduce repititive code

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/server.js
@@ -1,5 +1,6 @@
 import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
 
 const PORT = 8000
 
@@ -8,9 +9,7 @@ const server = http.createServer(async (req, res) => {
 
   if (req.url === '/api' && req.method === 'GET') {
 
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(destinations))
+    sendJSONResponse(res, 200, destinations)
 
   } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
 
@@ -18,21 +17,18 @@ const server = http.createServer(async (req, res) => {
     const filteredData = destinations.filter((destination) => {
       return destination.continent.toLowerCase() === continent.toLowerCase()
     })
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(filteredData))
+    sendJSONResponse(res, 200, filteredData)
 
   } else {
 
     res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.end(JSON.stringify({
+    sendJSONResponse(res, 404, ({
       error: "not found",
       message: "The requested route does not exist"
     })
     )
+    
   }
-  
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
