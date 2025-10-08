0da20dad8aa3ad2379e3806497a9fddaa87a0d81
Hamada <hamadauter@gmail.com>
2025-10-09 01:05:50 +0800

    Coded the sad path for a 404 page if the visited resource does not exist

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/server.js
@@ -1,20 +1,19 @@
 import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
- 
+
 const PORT = 8000
 
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB()
 
-
   if (req.url === '/api' && req.method === 'GET') {
-/*
-Challenge:
-1. Access the 'statusCode' property and set it to 200.
-*/
     res.setHeader('Content-Type', 'application/json')
     res.statusCode = 200
     res.end(JSON.stringify(destinations))
+  } else {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 404
+    res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
   }
 })
 
