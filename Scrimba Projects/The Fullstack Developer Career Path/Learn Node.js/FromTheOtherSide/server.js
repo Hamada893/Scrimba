b670099b9a91feb717a69cd117a2d2271d82640d
Hamada <hamadauter@gmail.com>
2025-10-13 12:46:09 +0800

    Learned and implemented about the FS Module and used it to serve index.html

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/server.js
@@ -3,26 +3,12 @@ import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
 
-/*
-Challenge 1:
-
-1. Get the name of the directory holding this server.js file and store it to a const ‘__dirname’.
-*/
-
 const __dirname = import.meta.dirname
 
+/* Make any changes needed to the code below. */
+const server = http.createServer(async (req, res) => {
+    await serveStatic(req, res, __dirname)
 
-const server = http.createServer((req, res) => {
-
-/*
-Challenge 3:
-
-1. Import and call serveStatic and pass it the directory of this current module.
-*/
-    serveStatic(__dirname)
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><h1>The server is working</h1></html>')
 })
 
 server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/utils/serveStatic.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/utils/serveStatic.js
@@ -1,20 +1,27 @@
-/*
-Challenge 2:
-
-1. Create and export a function called 'serveStatic'. 
-   It should take in the base directory as a parameter.
-
-2. Build a path to index.html in the 'public' folder and save it to a const 'filePath'. 
-   (Which node module will you need to import to do this? Which method joins the path together?)
-
-3. Log 'filePath' to the console.
-*/
-
 import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
 
-export function serveStatic(baseDir) {
-
+export async function serveStatic(req, res, baseDir) {
+  
   const filePath = path.join(baseDir, 'public', 'index.html')
-  console.log(filePath)
+
+  try {
+    const content = await fs.readFile(filePath)
+    sendResponse(res, 200, 'text/html', content)
+  } catch (err) {
+    console.log(err)
+  }
+
+/*
+Challenge 3:
+
+- Import sendResponse() and use it to serve index.html. 
+  Pass in all of the information sendResponse() is expecting.
+  serveStatic() will need another param. What is it?
+
+  Make any changes necessary in server.js and delete any unneeded code.
+
+*/
 
 }
