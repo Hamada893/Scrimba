51e86c0c655a103d0eb2e8604eacac2ae898a8c3
Hamada <hamadauter@gmail.com>
2025-10-13 19:51:19 +0800

    Used path.extname to get the file extension's name to serve the frontend

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/utils/serveStatic.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/utils/serveStatic.js
@@ -1,27 +1,33 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
 
 export async function serveStatic(req, res, baseDir) {
-  
-  const filePath = path.join(baseDir, 'public', 'index.html')
+/*
+Challenge: 
+  1. Write code below to serve files from our public directory.
+      
+      Don’t worry about handling errors for now.
+      hint.md for help!
+*/
+
+  const publicDir = path.join(baseDir, 'public')
+  const filePath = path.join(
+    publicDir,
+    req.url === '/' ? 'index.html' : req.url
+  )
+
+  const ext = path.extname(filePath)
+
+  const contentType = getContentType(ext)
 
   try {
     const content = await fs.readFile(filePath)
-    sendResponse(res, 200, 'text/html', content)
+    sendResponse(res, 200, contentType, content)
+
   } catch (err) {
     console.log(err)
   }
-
-/*
-Challenge 3:
-
-- Import sendResponse() and use it to serve index.html. 
-  Pass in all of the information sendResponse() is expecting.
-  serveStatic() will need another param. What is it?
-
-  Make any changes necessary in server.js and delete any unneeded code.
-
-*/
 
 }
