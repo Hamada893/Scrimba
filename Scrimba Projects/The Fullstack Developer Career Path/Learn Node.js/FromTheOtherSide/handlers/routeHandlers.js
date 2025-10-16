2d53e8a181076fb311dc7e70eb80e8c8b9dbed81
Hamada <hamadauter@gmail.com>
2025-10-16 09:31:15 +0800

    Added donkey logic to send a POST request + the route

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/handlers/routeHandlers.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/handlers/routeHandlers.js
@@ -5,17 +5,19 @@ export async function handleGet(res) {
   const data = await getData()
   const content = JSON.stringify(data)
   sendResponse(res, 200, 'application/json', content)
-}
+}  
+
+export function handlePost(req, res) {
+  console.log('POST request received')
+} 
+
+// parseJSONBody() will collect and parse the incoming JSON
+// santizeData() 
+// addNewSighting() will do the donkey work of adding the data to our dataset
+// sendResponse()
 
 /*
 Challenge:
-1. Export a function called handleGet(). 
-2. It should:
-   - use getData() to get the data
-   - stringify that data
-   - use sendResponse() to serve it
-
-Open the browser and load the sightings page to see if it works.
+  1. Create and export a function called handlePost().
+  2. For now, that function can just log 'POST request received'.
 */
-
-// handlePost
--- Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Node.js/FromTheOtherSide/server.js
@@ -1,28 +1,30 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
 import { handleGet } from './handlers/routeHandlers.js'
+import { handlePost } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-/*
-Challenge: 
-   1. Set up a route for ‘/api’.
-   2. Nest an if to check if the method is ‘GET’. 
-   3. When a GET request is received to '/api', use handleGet() to handle it.
-*/ 
 
     if (req.url === '/api') {
         if (req.method === 'GET') {
             return await handleGet(res)
         }
+        else if (req.method === 'POST') {
+            handlePost(req, res)
+        }
+/*
+Challenge: 
+   1. Add a route for a POST request to '/api'.
+   2. When a request comes in, pass the req and res to handlePost().
+*/
     }
     else if (!req.url.startsWith('/api')) {
-        return await serveStatic(req, res, __dirname) 
+        return await serveStatic(req, res, __dirname)
     }
-}) 
+})
 
-server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
- 
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
