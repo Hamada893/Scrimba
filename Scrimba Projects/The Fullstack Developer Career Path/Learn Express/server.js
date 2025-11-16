8423c1ccb07e78e14039a5ebbcc8a8fd44ac41fb
Hamada <hamadauter@gmail.com>
2025-11-17 07:59:04 +0800

    Learned how to serve data with Express

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Express/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Express/server.js
@@ -1,7 +1,14 @@
-console.log('We have a package.json')
+import express from 'express'
+import { startups } from './data/data.js'
 
-/*
-Challenge:
- 1. Create a package.json file by typing "npm init" in the terminal.
- 2. Answer the questions using the info in project_details.md.
-*/
+const PORT = 8000
+
+const app = express()
+
+app.get('/api', (req, res) => {
+  res.json(startups)
+})
+
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
+
