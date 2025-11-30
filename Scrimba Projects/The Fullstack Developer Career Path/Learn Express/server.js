5d61b8255b65ac64c0c3b69a1d9536cc55c7f1a8
Hamada <hamadauter@gmail.com>
2025-11-30 21:15:50 +0800

    Added logic to handle path parameters for different geo fields + countries

--- Scrimba Projects/The Fullstack Developer Career Path/Learn Express/server.js
+++ Scrimba Projects/The Fullstack Developer Career Path/Learn Express/server.js
@@ -12,39 +12,77 @@ app.get('/api', (req, res) => {
   const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
 
   if (industry) {
-    filteredData = filteredData.filter( startup => 
+    filteredData = filteredData.filter(startup =>
       startup.industry.toLowerCase() === industry.toLowerCase()
     )
   }
 
   if (country) {
-    filteredData = filteredData.filter( startup => 
+    filteredData = filteredData.filter(startup =>
       startup.country.toLowerCase() === country.toLowerCase()
     )
   }
-  
+
   if (continent) {
-    filteredData = filteredData.filter( startup => 
+    filteredData = filteredData.filter(startup =>
       startup.continent.toLowerCase() === continent.toLowerCase()
     )
   }
 
   if (is_seeking_funding) {
-    filteredData = filteredData.filter( startup => 
+    filteredData = filteredData.filter(startup =>
       startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
     )
   }
-  
+
   if (has_mvp) {
-    filteredData = filteredData.filter( startup => 
+    filteredData = filteredData.filter(startup =>
       startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
     )
   }
-
 
   res.json(filteredData)
 
 })
 
-app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
 
+app.get('/api/:field/:term', (req, res) => {
+
+  const { field, term } = req.params
+
+  const filteredData = startups.filter(
+    startup => startup[field].toLowerCase() === term.toLowerCase()
+  )
+
+  res.json(filteredData)
+
+})
+
+/*
+Challenge:
+1. Add a new route which accepts GET requests to /api/<field>/<term>.
+2. Filter the data based on the path params.
+3. Serve the filtered data.
+
+For now, don’t worry that using some fields will trigger an error.
+
+** The functionality **
+Get all startups in a given country via api/country/<country name>
+Get all startups in a given continent via api/continent/<continent name>
+Get all startups in a given industry via api/industry/<industry name>
+
+**Test Cases** 
+
+These should work:
+  api/country/india
+  api/continent/europe
+  api/industry/ai
+
+This will throw an error - but that's fine!
+	api/has_mvp/true
+
+*/
+
+
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
